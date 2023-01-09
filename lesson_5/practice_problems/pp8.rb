8b29468f30385006d57cc6dd73bedd43081114a5
Saurabh Kamboj <skamboj97@gmail.com>
2023-01-09 08:12:29 +0530

    Add files for practice problems 1-8

---
+++ lesson_5/practice_problems/pp8.rb
@@ -0,0 +1,22 @@
+hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'], fourth: ['over', 'the', 'lazy', 'dog']}
+
+# Using the each method, write some code to output all of the vowels from the strings.
+
+hsh.each do |key, value|
+  value.each do |string|
+    string.chars.each do |char|
+      puts char if %w(a e i o u).any?(char)
+    end
+  end
+end
+
+# alternate
+vowels = 'aeiou'
+
+hsh.each do |_, value|
+  value.each do |str|
+    str.chars.each do |char|
+      puts char if vowels.include?(char)
+    end
+  end
+end
